db13dabea83be50989a04a917d86fbb56cfc4f6f
xxyCoder <109936101+xxyCoder@users.noreply.github.com>
2022-11-27 13:27:46 +0800

    小程序
    新增 数据绑定 列表渲染之wx:for 页面跳转

--- fromBook/pages/post/post.js
+++ fromBook/pages/post/post.js
@@ -1,7 +1,65 @@
 Page({
-    data:{},
+    data:{
+        // object: {
+        //     date:"Jan 28 2020"
+        // },
+        // title:"汽车",
+        // postImg:"https://p3.dcarimg.com/img/motor-img/e1acc27d3434f67503bde03ba01408fa~332x0.webp",
+        
+        // readingNum:108,
+        // collectionNum:92,
+        // commentNum:{
+        //     array:[97]
+        // }
+    },
     onLoad:function(options){
         console.log("OnLoad页面被加载");
+        
+        var postList = [{
+            object: {
+                date:"Jan 28 2020"
+            },
+            title:"汽车",
+            postImg:"https://p3.dcarimg.com/img/motor-img/e1acc27d3434f67503bde03ba01408fa~332x0.webp",
+            content:"看车就上懂车帝",
+            readingNum:108,
+            collectionNum:92,
+            commentNum:{
+                array:[97]
+            }
+        },{
+            object: {
+                date:"Jan 28 2020"
+            },
+            title:"汽车",
+            postImg:"https://p3.dcarimg.com/img/motor-img/e1acc27d3434f67503bde03ba01408fa~332x0.webp",
+            content:"看车就上懂车帝",
+            readingNum:118,
+            collectionNum:109,
+            commentNum:{
+                array:[17]
+            }
+        },{
+            object: {
+                date:"Jan 28 2020"
+            },
+            title:"汽车",
+            postImg:"https://p3.dcarimg.com/img/motor-img/e1acc27d3434f67503bde03ba01408fa~332x0.webp",
+            content:"看车就上懂车帝",
+            readingNum:1208,
+            collectionNum:92,
+            commentNum:{
+                array:[937]
+            }
+        }
+    ]
+    this.setData({
+        title:"一辆汽车的发展史",
+        "commentNum.array[0]":100,
+        "object.date":"Jan 29 2020",
+        // content:"看车就上懂车帝"
+        postList:postList
+    })
     },
     onShow:function(){
         console.log("onShow页面被显示");
@@ -15,4 +73,5 @@ Page({
     onUnload:function(){
         console.log("onLoad页面被卸载");
     }
+    
 })
--- fromBook/pages/welcom/welcom.js
+++ fromBook/pages/welcom/welcom.js
@@ -1,3 +1,22 @@
 Page({
-    
+    onTapJump:function(event) {
+        wx.navigateTo({
+          url: '../post/post',
+          success:function(){
+              console.log('jump success');
+          },
+          fail:function(){
+            console.log('jump fail');
+          },
+          complete:function() {
+              console.log('jump complete');
+          }
+        })
+    },
+    onUnload:function(){
+        console.log('welcome页面被卸载');
+    },
+    onHide:function(){
+        console.log('welcome页面被隐藏');
+    }
 })
