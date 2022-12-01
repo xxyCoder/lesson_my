1b23fb2489e385738b559b72c46bc3460d0b7e54
xxyCoder <109936101+xxyCoder@users.noreply.github.com>
2022-12-01 15:18:06 +0800

    王者荣耀
    添加事件与滚动条

--- weapp/wangzherongyao/pages/index/index.js
+++ weapp/wangzherongyao/pages/index/index.js
@@ -10,18 +10,37 @@ Page({
     },
     // 生命周期事件
     onLoad() {
+        wx.showLoading({
+          title: '加载中....',
+        })
         // 从客户端 去到 服务器端获取数据
         // 微信wx request
         wx.request({
             url: 'https://www.fastmock.site/mock/3ddf189404196ed14cb6531bf3596081/vue/hero',
             // 成功请求后
-            success: function(res) {
-                // 解析res数据
-                let nav = res.data.nav;
-                let list = res.data.heroList;
+            success:(res) => {
+                wx.hideLoading();
+                // statusCode == 200 表示服务器端没有任何错误
+                // if (res.statusCode === 200) {
+                //     // 解析res数据
+                //     let nav = res.data.nav;
+                //     let list = res.data.heroList;
+                // }
+                if (res.errMsg == "request:ok") {
+                    let data = res.data;
+                    // es6 语法之解构，从data中拿出一个nav
+                    let { nav,heroList } = data;
+                    // 设置数据
+                    // this 指向page对象
+                    // 设置的同时，自动渲染模板
+                    this.setData ({
+                        nav: nav,
+                        heroList: heroList
+                    })
+                }
             },
             // 失败后回调
-            fail:() => {
+            fail: () => {
                 console.log("error");
             }
         })
